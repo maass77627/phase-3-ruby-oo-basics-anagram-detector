b4802e31e12a9cf96057218614b276a9408a2024
kelly_maass <kmaass77627@gmail.com>
2024-06-29 23:19:36 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+def initialize(word)
+    @word = word
+end
+
+def match(anagrams)
+    answer = []
+    anagrams.each do |word|
+        array = word.split("")
+        arraytwo = @word.split("")
+        if array.sort == arraytwo.sort
+            answer << array.join("")
+        end
+
+end
+answer
+end
+
+end
+
